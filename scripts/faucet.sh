23a0b45aaed8a1e142288ba18b9ee006c2b6e9f8
0x6368616e67 <0x6368616e67@gmail.com>
2022-08-19 06:32:10 +0000

    feat: add transaction

---
+++ scripts/faucet.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+curl --request POST \
+  --url 'https://faucet.devnet.aptoslabs.com/mint?address=0xb5b3f30964642ff9406c092e89f320ace3ec8508e039b41f66adf7d466d52df9&amount=10000000'
+
--- types/transaction.go
+++ types/transaction.go
@@ -1,1 +1,31 @@
 package types
+
+import "github.com/0x6368616e67/aptos-sdk-go/client"
+
+type TransactionPayload struct {
+	Type          string        `json:"type"`
+	Function      string        `json:"function"`
+	TypeArguments []string      `json:"type_arguments"`
+	Arguments     []interface{} `json:"arguments"`
+}
+
+type TransactionSignature struct {
+	Type      string `json:"type"`
+	PublicKey string `json:"public_key"`
+	Signature string `json:"signature"`
+}
+
+type Transaction struct {
+	Sender                  string               `json:"sender"`
+	SequenceNumber          string               `json:"sequence_number"`
+	MaxGasAmount            string               `json:"max_gas_amount"`
+	GasUnitPrice            string               `json:"gas_unit_price"`
+	ExpirationTimestampSecs string               `json:"expiration_timestamp_secs"`
+	Payload                 TransactionPayload   `json:"payload"`
+	Signature               TransactionSignature `json:"signature"`
+	SecondarySigners        []string             `json:"secondary_signers"`
+}
+
+func (tx *Transaction) Encode(cli *client.Client) (data []byte, err error) {
+
+}
